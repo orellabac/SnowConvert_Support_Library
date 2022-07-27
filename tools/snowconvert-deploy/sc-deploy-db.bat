84c3359dadef7a96a343d620cff48c9d5650f97b
Orellabac <orellabac@gmail.com>
2022-07-27 16:56:03 -0600

    adding quotes to avoid issues with spaces

--- tools/snowconvert-deploy/sc-deploy-db.bat
+++ tools/snowconvert-deploy/sc-deploy-db.bat
@@ -4,6 +4,6 @@ REM Set the python io encoding to UTF-8 by default if not set.
 IF "%PYTHONIOENCODING%"=="" (
     SET PYTHONIOENCODING="UTF-8"
 )
-python %~dp0\sc-deploy-db %*
+python "%~dp0\sc-deploy-db" %*
 
 endlocal
